59096f40cdf8099171e8ef71c239bbed59eb8120
Connor White <connor.bcw@gmail.com>
2020-09-23 13:47:30 -0500

    fix: extra space after command with no args

--- source/utils.ts
+++ source/utils.ts
@@ -2,5 +2,5 @@ import { Executable } from "./";
 import { getArgs } from "./args";
 
 export function executableToString({ command, args }: Executable) {
-  return `${command} ${getArgs(args).join(" ")}`;
+  return [command].concat(getArgs(args)).join(" ");
 }
